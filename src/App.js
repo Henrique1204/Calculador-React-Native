beeebf40d843782eb54d2ac1ab0a9204dbdb7f35
Paulo <pauloh16.pdr@gmail.com>
2021-09-12 21:30:24 -0300

    Adicionar estilos para botes

--- src/App.js
+++ src/App.js
@@ -5,30 +5,34 @@ import Botao from './componentes/Botao';
 import Display from './componentes/Display';
 
 const App = () => {
-  const [valor, setValor] = useState(0);
+  const [valor, setValor] = useState('0');
+
+  const addDigito = (digito) => setValor((valor) => valor === '0' ? digito : valor + digito);
+  const limparDigitos = () => setValor('0');
+  const setOperacao = (operecao) => console.warn(operecao);
 
   return (
     <SafeAreaView style={estilos.App}>
       <Display valor={valor} />
 
       <View style={estilos.botoes}>
-        <Botao texto='AC' />
-        <Botao texto='/' />
-        <Botao texto='7' />
-        <Botao texto='8' />
-        <Botao texto='9' />
-        <Botao texto='*' />
-        <Botao texto='4' />
-        <Botao texto='5' />
-        <Botao texto='6' />
-        <Botao texto='-' />
-        <Botao texto='1' />
-        <Botao texto='2' />
-        <Botao texto='3' />
-        <Botao texto='+' />
-        <Botao texto='0' />
-        <Botao texto='.' />
-        <Botao texto='=' />
+        <Botao texto='AC' estilo='botaoTriplo' onPress={limparDigitos} />
+        <Botao texto='/' estilo='operacao' onPress={setOperacao} />
+        <Botao texto='7' onPress={addDigito} />
+        <Botao texto='8' onPress={addDigito} />
+        <Botao texto='9' onPress={addDigito} />
+        <Botao texto='*' estilo='operacao' onPress={setOperacao} />
+        <Botao texto='4' onPress={addDigito} />
+        <Botao texto='5' onPress={addDigito} />
+        <Botao texto='6' onPress={addDigito} />
+        <Botao texto='-' estilo='operacao' onPress={setOperacao} />
+        <Botao texto='1' onPress={addDigito} />
+        <Botao texto='2' onPress={addDigito} />
+        <Botao texto='3' onPress={addDigito} />
+        <Botao texto='+' estilo='operacao' onPress={setOperacao} />
+        <Botao texto='0' estilo='botaoDuplo' onPress={addDigito} />
+        <Botao texto='.' onPress={addDigito} />
+        <Botao texto='=' estilo='operacao' onPress={setOperacao} />
       </View>
     </SafeAreaView>
   );
--- src/componentes/Botao.js
+++ src/componentes/Botao.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 
 const estilos = StyleSheet.create({
-    button: {
+    botao: {
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
         padding: 20,
@@ -16,13 +16,23 @@ const estilos = StyleSheet.create({
         fontSize: 40,
         textAlign: 'center',
         backgroundColor: '#F0F0F0'
+    },
+    operacao: {
+        color: '#FFF',
+        backgroundColor: '#FA8231'
+    },
+    botaoDuplo: {
+        width: Dimensions.get('window').width / 2,
+    },
+    botaoTriplo: {
+        width: (Dimensions.get('window').width / 4) * 3,
     }
 });
 
-const Botao = ({ onPress, texto }) => {
+const Botao = ({ onPress, texto, estilo }) => {
     return (
-        <TouchableHighlight onPress={onPress}>
-            <Text style={estilos.button}>{texto}</Text>
+        <TouchableHighlight onPress={() => onPress(texto)}>
+            <Text style={{ ...estilos.botao, ...estilos[estilo] }}>{texto}</Text>
         </TouchableHighlight>
     );
 };
